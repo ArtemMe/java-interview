e11fc30b158213556694df64152172211f4e90ff
Артем Межеловский <amejelovskiy@mmtr.ru>
2018-12-31 12:30:00 +0300

    snail

---
+++ src/main/java/codewars/snail/SnailTest.java
@@ -0,0 +1,33 @@
+package codewars.snail;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runners.JUnit4;
+import java.util.Arrays;
+import java.util.Random;
+import static java.util.stream.Collectors.joining;
+
+public class SnailTest {
+
+    @Test
+    public void SnailTest1() {
+        int[][] array
+                = {{1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}};
+        int[] r = {1, 2, 3, 6, 9, 8, 7, 4, 5};
+        test(array, r);
+    }
+
+    public String int2dToString(int[][] a) {
+        return Arrays.stream(a).map(row -> Arrays.toString(row)).collect(joining("\n"));
+    }
+
+    public void test(int[][] array, int[] result) {
+        String text = int2dToString(array) + " should be sorted to " + Arrays.toString(result);
+        System.out.println(text);
+        Assert.assertArrayEquals( result, Snail.snail(array));
+    }
+
+
+}
